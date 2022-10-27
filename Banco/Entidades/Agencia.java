7eb476df3856775e1fcef36b6c5f1cd3188fd56f
Rubens carvalho <rubenscarvalhofilho15@gmail.com>
2022-10-27 09:43:29 -0300

    Banco em java

---
+++ Banco/Entidades/Agencia.java
@@ -0,0 +1,54 @@
+package Banco.Entidades;
+
+public class Agencia {
+    private int numero;
+    private String telefone;
+    private String email;
+    private Endereco endereco;
+
+    public Agencia() {
+    }
+
+    public Agencia(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Agencia(int numero, String telefone, String email) {
+        this.numero = numero;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
