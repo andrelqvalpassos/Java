bff3e3ad99eb0f656f5ce320b44428f80b34709e
andrevalpassos <111320693+andrevalpassos@users.noreply.github.com>
2022-11-07 21:32:35 -0300

    Atividade faculdade

---
+++ atividades/Fornecedor.java
@@ -0,0 +1,34 @@
+package atividades;
+
+public class Fornecedor {
+    private String CNPJ;
+    private String telefone;
+    private String nome;
+    
+    public Fornecedor() {
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String cNPJ) {
+        CNPJ = cNPJ;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
